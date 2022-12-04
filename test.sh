78711bbbbd2a1b7fb2d4fca5eebf6ba7b74b9085
Yoav Klein <yoavklein25@gmail.com>
2022-12-04 20:19:51 +0000

    dev

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+ns_name="blue"
+cntainer_id="mynewcontainer"
+if_name="veth0"
 
+clean() {
+    sudo ip netns delete "$ns_name"
+    sudo rm -rf /var/run/netns/*
+}
 
 test_add() {
     echo "=== Testing add"
@@ -8,12 +15,12 @@ test_add() {
     derived=$(echo $config | jq '. += .plugins[0] | del(.plugins)')
     
     # create אthe red namespace
-    sudo ip netns | grep "red" || sudo ip netns add red
+    sudo ip netns | grep "$ns_name" || sudo ip netns add "$ns_name"
 
     export CNI_COMMAND=ADD
-    export CNI_NETNS=/var/run/netns/red
-    export CNI_CONTAINERID="mynewcontainer"
-    export CNI_IFNAME="veth0"
+    export CNI_NETNS="/var/run/netns/$ns_name"
+    export CNI_CONTAINERID="$container_id"
+    export CNI_IFNAME="$if_name"
 
     echo $derived | sudo -E ./my-cni-plugin
 }
@@ -26,5 +33,6 @@ usage() {
 case $1 in
 add) test_add ;;
 del) test_del ;;
+clean) clean ;;
 *) usage
 esac
