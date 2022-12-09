cba5f560d1e3dc657fbf4230374c111df4018f9b
Yoav Klein <yoavklein25@gmail.com>
2022-12-09 12:15:52 +0200

    fix

--- echo-server/echo.py
+++ echo-server/echo.py
@@ -1,3 +1,4 @@
+import socket
 
 from flask import Flask, request
 
@@ -5,7 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return f"Hello, received request from {request.remote_addr}\n"
+    host = socket.gethostname()
+    ip = socket.gethostbyname(host)
+    return f"Hello {request.remote_addr}, this is {ip}\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
